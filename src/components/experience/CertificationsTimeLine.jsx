92909dbc3df0ecb579c9368ee5e01c66da2793d9
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-09-23 19:18:35 -0500

    Glow animation

--- src/components/experience/CertificationsTimeLine.jsx
+++ src/components/experience/CertificationsTimeLine.jsx
@@ -6,38 +6,55 @@ import { SiOpenbadges } from 'react-icons/si';
 const CertificationsTimeLine = () => (
   <>
     <VerticalTimeline>
+
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
-        contentStyle={{ background: 'rgb(39 39 39 / 56%)', color: '#fff' }}
+        contentStyle={{ backgroundColor: 'rgb(39 39 39 / 56%)', color: '#fff' }}
         contentArrowStyle={{ borderRight: '7px solid  white' }}
-        date="2011 - present"
-        iconStyle={{ transform: 'scale(0.9)', background: 'rgb(255 143 0)', color: '#fff' }}
+        date="january 2021 – january 2022"
+        iconStyle={{ transform: 'scale(0.9)', backgroundColor: 'rgb(22 22 22)', color: '#fff' }}
         icon={<SiOpenbadges />}
       >
-        <h3 className="vertical-timeline-element-title">Creative Director</h3>
-        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-        <p>
-          Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+        <h3 className="vertical-timeline-element-title">Front-End Developer</h3>
+        <p className="timeline-name">Chicken Wings: The Prince</p>
+        <p className="vertical-timeline-element-subtitle">Medellin, Colombia</p>
+        <p className="timeline-text">
+          JavaScript, React, HTML, CSS, SEO, UI/UX, Frameworks, Git, Web Design, Web Development
         </p>
       </VerticalTimelineElement>
 
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
-        contentStyle={{ background: 'rgb(39 39 39 / 56%)', color: '#fff' }}
-        date="2010 - 2011"
-        iconStyle={{ transform: 'scale(0.9)', background: 'rgb(255 143 0)', color: '#fff' }}
+        contentStyle={{ backgroundColor: 'rgb(39 39 39 / 56%)', color: '#fff' }}
+        contentArrowStyle={{ borderRight: '7px solid  white' }}
+        date="march 2022 - january 2023"
+        iconStyle={{ transform: 'scale(0.9)', backgroundColor: 'rgb(22 22 22)', color: '#fff' }}
         icon={<SiOpenbadges />}
       >
-        <h3 className="vertical-timeline-element-title">Art Director</h3>
-        <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-        <p>
-          Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+        <h3 className="vertical-timeline-element-title">Technical Assistant</h3>
+        <p className="timeline-name">[24]7.ai</p>
+        <p className="vertical-timeline-element-subtitle">San Jose, CA (remote)</p>
+        <p className="timeline-text">
+          Team Leadership, Technical Support, Customer Service,
+          Multitasking, Bilingual, Problem Solver
         </p>
       </VerticalTimelineElement>
 
       <VerticalTimelineElement
-        iconStyle={{ opacity: '0', transform: 'scale(0.9)', animation: 'none' }}
-      />
+        className="vertical-timeline-element--work"
+        contentStyle={{ backgroundColor: 'rgb(39 39 39 / 56%)', color: '#fff' }}
+        contentArrowStyle={{ borderRight: '7px solid  white' }}
+        date="march 2023 - present"
+        iconStyle={{ transform: 'scale(0.9)', backgroundColor: 'rgb(22 22 22)', color: '#fff' }}
+        icon={<SiOpenbadges />}
+      >
+        <h3 className="vertical-timeline-element-title">Mentor</h3>
+        <p className="timeline-name">Microverse</p>
+        <p className="vertical-timeline-element-subtitle">San Francisco, CA (remote)</p>
+        <p className="timeline-text">
+          Team Leadership, Full-Stack Development, Agile Methodologies, Pair Programming, Git
+        </p>
+      </VerticalTimelineElement>
 
     </VerticalTimeline>
   </>
